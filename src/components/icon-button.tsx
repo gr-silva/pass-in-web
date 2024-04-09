a82be84a5fae553c87dcc402b621a7ebe29c3288
Gabriel Rocha <gabriel@t2tsolucoes.com>
2024-04-09 00:01:41 -0300

    feat: add IconButton component

---
+++ src/components/icon-button.tsx
@@ -0,0 +1,16 @@
+import { ComponentProps } from "react";
+
+interface IconButtonProps extends ComponentProps<"button"> {
+  transparent?: boolean
+}
+
+export function IconButton({ transparent, ...props }: IconButtonProps) {
+  return (
+    <button
+      {...props}
+      className={transparent
+        ? "bg-black/20 border border-white/10 rounded-md p-1.5"
+        : "rounded-md border border-white/10 bg-white/10 p-1.5"}
+    />
+  );
+}
